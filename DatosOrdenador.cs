1fe9c00692bb695c91de5ca9a8c59272350c3004
MINEDUCYT <MINEDUCYT@DESKTOP-CC30LNJ>
2025-02-20 23:01:22 -0600

    Agregar archivos de proyecto.

---
+++ DatosOrdenador.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2._3_20253446
+{
+    public class DatosOrdenador
+    {
+        public DatosOrdenador(string marca, string procesador, string memoria, string discoduro)
+        {
+            this.Marca = marca;
+            this.Procesador = procesador;
+            this.Memoria = memoria;
+            this.Discoduro = discoduro;
+
+        }
+        public string Marca;
+        public string Procesador;
+        public string Memoria;
+        public string Discoduro;
+
+        public void MayorCapacidad(string texto)
+        {
+            Console.WriteLine("La mayor capacidad del ordenador es 100 en 100GB");
+        }
+        public void MenorCapacidad(string texto)
+        {
+            Console.WriteLine("la menor capacidad del ordenaodor es menor de 100GB");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2._3_20253446
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DatosOrdenador datosOrdenador = new DatosOrdenador("HP", "INTEL core 5", "1TB", "SSD");
+            Console.WriteLine("Descripción del ordenador: ");
+            Console.WriteLine("Marca " + datosOrdenador.Marca);
+            Console.WriteLine("Procesador " + datosOrdenador.Procesador);
+            Console.WriteLine("Memoria " + datosOrdenador.Memoria);
+            Console.WriteLine("Discoduro " + datosOrdenador.Discoduro);
+
+            datosOrdenador.MayorCapacidad("");
+            datosOrdenador.MenorCapacidad("");
+            Console.ReadKey();
+        }
+    }
+}
+
